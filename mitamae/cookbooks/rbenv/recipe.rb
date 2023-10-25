1195b33e0ee0641225b9dfc0aff22ecbfe59117b
jagio <jagio0129@gmail.com>
2023-10-25 20:58:01 +0900

    インストール済みだったら再実行しないように修正

--- mitamae/cookbooks/rbenv/recipe.rb
+++ mitamae/cookbooks/rbenv/recipe.rb
@@ -2,16 +2,25 @@ package 'rbenv'
 
 anyenv_dir = "/Users/jagio/.anyenv"
 
+# install rbenv
 execute 'anyenv install rbenv' do
   not_if "test #{anyenv_dir}/.anyenv/envs/rbenv"
   not_if "brew list | grep anyenv"
 end
 
+# install default version
 default_version = node[:rbenv][:default]
-execute "rbenv install #{default_version}"
-execute "rbenv global #{default_version}"
+execute "rbenv install #{default_version}" do
+  not_if "rbenv versions | grep #{default_version}"
+end
+execute "rbenv global #{default_version}" do
+  not_if "rbenv global | grep #{default_version}"
+end
 
+# install others version
 other_versions = node[:rbenv][:others]
 other_versions.each do |version|
-  execute "rbenv install #{version}"
+  execute "rbenv install #{version}" do
+    not_if "rbenv versions | grep #{version}"
+  end
 end
