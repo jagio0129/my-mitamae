1c1661177846a7a86b07d70ed14616e70b8d18b4
jagio <jagio0129@gmail.com>
2023-10-27 14:15:11 +0900

    add tmux recipe

--- mitamae/helper.rb
+++ mitamae/helper.rb
@@ -3,4 +3,11 @@ MItamae::RecipeContext.class_eval do
     root = File.expand_path('..', __FILE__)
     include_recipe File.join(root, 'cookbooks', name, 'recipe')
   end
+
+  def brew_package(name)
+    execute "install #{name}" do
+      command "brew install #{name}"
+      not_if "brew list | grep #{name}"
+    end
+  end
 end
