38c9fa19216adee753ce21bbb6cc88cc1dbec3c8
jagio <jagio0129@gmail.com>
2024-05-14 15:30:16 +0900

    refactor: quite abstraction

--- mitamae/cookbooks/goenv/recipe.rb
+++ mitamae/cookbooks/goenv/recipe.rb
@@ -1,1 +1,25 @@
-hogenv 'goenv'
+anyenv_dir = "#{node[:home]}/.anyenv"
+
+# install
+execute "anyenv install goenv" do
+  not_if "anyenv version | grep goenv"
+end
+
+# install default version
+default_version = node[:goenv][:default]
+execute "goenv install #{default_version}" do
+  not_if "goenv versions | grep #{default_version}"
+end
+execute "goenv global #{default_version}" do
+  not_if "goenv global | grep #{default_version}"
+end
+
+# install others version
+other_versions = node[:goenv][:others]
+return if other_versions.nil?
+
+other_versions.each do |version|
+  execute "goenv install #{version}" do
+    not_if "goenv versions | grep #{version}"
+  end
+end
--- mitamae/cookbooks/rbenv/recipe.rb
+++ mitamae/cookbooks/rbenv/recipe.rb
@@ -1,1 +1,25 @@
-hogenv 'rbenv'
+anyenv_dir = "#{node[:home]}/.anyenv"
+
+# install
+execute "anyenv install rbenv" do
+  not_if "anyenv version | grep rbenv"
+end
+
+# install default version
+default_version = node[:rbenv][:default]
+execute "rbenv install #{default_version}" do
+  not_if "rbenv versions | grep #{default_version}"
+end
+execute "rbenv global #{default_version}" do
+  not_if "rbenv global | grep #{default_version}"
+end
+
+# install others version
+other_versions = node[:rbenv][:others]
+return if other_versions.nil?
+
+other_versions.each do |version|
+  execute "rbenv install #{version}" do
+    not_if "rbenv versions | grep #{version}"
+  end
+end
