96f65477b5093765dbbb77be74a66bd5a4aedb25
jagio <jagio0129@gmail.com>
2024-05-14 17:23:12 +0900

    add yabai

---
+++ mitamae/cookbooks/yabai/recipe.rb
@@ -0,0 +1,9 @@
+package 'koekeishiya/formulae/yabai'
+package 'koekeishiya/formulae/skhd'
+
+# https://github.com/koekeishiya/yabai
+# https://zenn.dev/yootaki/articles/d6ba758c63a315
+#
+# consoleで `yabai --start-service`とすると、アクセシビリティでyabaiを許可できるようになる
+# consoleで `skhd --start-service`とすると、アクセシビリティでskhdを許可できるようになる
+# 起動、停止は上記コマンドで
